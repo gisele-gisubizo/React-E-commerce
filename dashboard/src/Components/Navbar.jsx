076db29e7a1e255fb67b2fda276aab373c2df20a
Gisubizo1005200 <gisubizoclasswork@gmail.com>
2025-02-18 10:04:46 +0200

    navbar added

--- dashboard/src/Components/Navbar.jsx
+++ dashboard/src/Components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import '../Styles/navbar.css'
 function Navbar() {
   return (
-    <div>
+    <div className='container-navbar'>
 
+            <div className='tittle'> 
+              
+                 <p> <span className='different-tittle'>envato</span>market</p>
+               
 
+               <div className='tittle-button'><button type='submit'>Buy now</button></div>
+           </div>
         
     </div>
   )
