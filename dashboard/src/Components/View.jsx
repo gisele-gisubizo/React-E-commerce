ae5e54327a59ef4c6b74f83c0d43bf761cc88a8f
Gisubizo1005200 <gisubizoclasswork@gmail.com>
2025-02-21 10:39:01 +0200

    adding boxes and their icons

--- dashboard/src/Components/View.jsx
+++ dashboard/src/Components/View.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoMenu } from "react-icons/io5";
 import '../Styles/view.css'
-
+import { CiWallet } from "react-icons/ci";
 import { IoSettings } from "react-icons/io5";
 import { MdWbSunny } from "react-icons/md";
 import { LuMoonStar } from "react-icons/lu"
@@ -11,7 +11,11 @@ import { AiTwotoneThunderbolt } from "react-icons/ai";
 import { IoNotifications } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
 import { PiTranslateFill } from "react-icons/pi";
+import plane from '../assets/plane.png'
 import admin from '../assets/admin.jpg';
+import { FaRegFileAlt } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { MdOutlineDownloadForOffline } from "react-icons/md";
 
 function View() {
 
@@ -46,6 +50,50 @@ function View() {
         setIsOpen(!isOpen);
 
     }
+
+
+const Array=[
+  {
+    id:1,
+    name:"All Earnings",
+    icon :[<CiWallet/>],
+    numbers:"$30200",
+    rate:" 30.6%",
+    // graph:,
+
+  },
+
+  {
+    id:2,
+    name:"All Earnings",
+    icon:[<FaRegFileAlt/>],
+    numbers:"$30200",
+    rate:" 30.6%",
+    // graph:,
+
+  },
+
+  {
+    id:3,
+    name:"All Earnings",
+    numbers:"$30200",
+    icon:[<SlCalender/>],
+    rate:" 30.6%",
+    // graph:,
+
+  },
+
+  {
+    id:4,
+    name:"All Earnings",
+  icon:[<MdOutlineDownloadForOffline/>],
+    numbers:"$30200",
+    rate:" 30.6%",
+    // graph:,
+
+  }
+]
+
   return (
  <div className={`container-sidebar ${isDarkTheme ? 'dark' : 'light'}`}>
       <div className='view-container'>
@@ -87,6 +135,46 @@ function View() {
    <div className='view-admin-image'><img src={admin} alt="admin's image"  className='view-image'/></div>
       </div>
       
+
+<div className='board'>
+
+<div className='board-content'>
+  <h5>Explore Redesigned Able <br /> Pro</h5>
+  
+  <p>The Brand new User Interface with power of Bootstrap <br /> Components. Explore the Endless possibilities with Able <br />Pro.</p>
+  
+  <button type='submit'>Exclusive on Themeforest</button>
+</div>
+
+<div className='image-board' ><img src={plane} alt="board image"/></div>
+
+</div>
+
+ 
+
+
+{
+             Array.map((value) => (
+                <>
+              <div key={value.id} className='box'>
+                     
+             <div className='array-box'>
+             <div >{value.icon}  </div>
+               <div >{value.name}  </div>
+               <div>{value.numbers}</div>                  
+                           <div>{value.rate}</div>
+               
+             </div>
+                  
+              </div>
+                    
+                    
+           
+                </>
+             ))
+             }
+
+
       </div>
 </div>
   )
